e183ec94e4d60420e33e05893ea47e0db727a19c
Diego Aquino <diegol.aquino@gmail.com>
2023-06-03 17:40:25 -0300

    modelagem

---
+++ MyWallet.Shared/DTO/CreateUserDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWallet.Shared.DTO
+{
+    public class CreateUserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordCopy { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CPF { get; set; }
+    }
+
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string CPF { get; set; }
+    }
+
+}
