2bf53adb17f7c81ddafcc9f1fc4715a792a004fc
Diego Aquino <diegol.aquino@gmail.com>
2023-11-14 23:36:12 -0300

    add new migrations

---
+++ MyWallet.Data/Migrations/20231113172656_GoalsWallet.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWallet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GoalsWallet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.RenameColumn(
+            //    name: "Installments",
+            //    table: "Incomes",
+            //    newName: "InstallmentsQuantity");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShoppingDay",
+                table: "Wallets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WalletType",
+                table: "Wallets",
+                type: "int",
+                nullable: true);
+
+            //migrationBuilder.AddColumn<int>(
+            //    name: "Installment",
+            //    table: "Incomes",
+            //    type: "int",
+            //    nullable: false,
+            //    defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShoppingDay",
+                table: "Wallets");
+
+            migrationBuilder.DropColumn(
+                name: "WalletType",
+                table: "Wallets");
+
+            migrationBuilder.DropColumn(
+                name: "Installment",
+                table: "Incomes");
+
+            migrationBuilder.RenameColumn(
+                name: "InstallmentsQuantity",
+                table: "Incomes",
+                newName: "Installments");
+        }
+    }
+}
---
+++ MyWallet.Data/Migrations/20231115022822_GoalsEntity.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWallet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GoalsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    Limit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Goals_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+               name: "Goals");
+        }
+    }
+}
