c087a6ed7881988d90e5c53cc650da07b6a3293b
Diego Aquino <diegol.aquino@gmail.com>
2023-12-23 22:54:14 -0300

    add exercise class

---
+++ MyWallet.Domain/Enums/EExerciseType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWallet.Domain.Enums
+{
+    public enum EExerciseType
+    {
+        Running = 1,
+        Swimming = 2,
+        Cycling = 3,
+        CrossFit = 4,
+        StrengthTraining = 5,
+        Soccer = 6,
+        Tenis = 7
+    }
+}
---
+++ MyWallet.Domain/Models/Exercise.cs
@@ -0,0 +1,28 @@
+﻿using MyWallet.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWallet.Domain.Models
+{
+    public class Exercise : BaseEntity
+    {
+        public double Distance { get; set; }   
+        public TimeSpan Duration { get; set; } 
+        public string Intensity { get; set; }  
+        public EExerciseType ExerciseType { get; set; }
+        public int Repetitions { get; set; }  
+        public string Tags { get; set; }
+        public Exercise(double distance, TimeSpan duration, string intensity, EExerciseType exerciseType, int repetitions, string tags)
+        {
+            Distance = distance;
+            Duration = duration;
+            Intensity = intensity;
+            ExerciseType = exerciseType;
+            Repetitions = repetitions;
+            Tags = tags;
+        }
+    }
+}
